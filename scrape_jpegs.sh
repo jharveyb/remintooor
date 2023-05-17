cf1536d92aa7ba3a02fcead42a9fb3c5bb56808a
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2023-05-16 22:35:35 -0400

    scrape images

---
+++ scrape_jpegs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Fetch a list of images, with numerical incrementing suffixes
+
+STARTVAL=0
+ENDVAL=10000
+
+echo "Image count: $ENDVALEXCLUDING"
+
+PATH_SUFFIX="cryptopunk"
+FILE_TYPE=".png"
+OUT_DIR=$(pwd)/$PATH_SUFFIX/
+
+# Create output dir
+if [[ ! -d $OUT_DIR ]]; then
+        mkdir -p "$OUT_DIR"
+fi
+
+echo "Output dir: $OUT_DIR"
+
+# Fill out URLs and paths
+BASE_URL="https://cryptopunks.app/cryptopunks/"
+EXTENDED_URL=$BASE_URL$PATH_SUFFIX
+
+for ((i=STARTVAL; i<ENDVAL; i++)); do
+        FULL_URL=$EXTENDED_URL$i$FILE_TYPE
+        FILE_NAME=$OUT_DIR$i$FILE_TYPE
+        wget -qO "$FILE_NAME" "$FULL_URL"
+
+        if ((i % 100 == 0)); then
+                echo "Finished $i images."
+        fi
+done
+
+echo "Scraped $ENDVAL images."
